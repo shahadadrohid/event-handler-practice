3fb1258af91ed16e7e963a904ea76a8f57d76ddd
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-06-27 19:46:05 +0600

    banking commit

--- js/banking.js
+++ js/banking.js
@@ -19,8 +19,33 @@ function getTotal(inputamount, currentAmount) {
     const currentTotal = currentTotalAmount + inputamount;
     getCurrentAmount.innerText = currentTotal;
 }
+
+function getTotalBalance(newInputValue, getBalance, isValue) {
+
+    // Get the current Balance 
+    const currentBalance = document.getElementById(getBalance);
+    const currentBalanceAmount = parseFloat(currentBalance.innerText);
+
+    // Adding current balance with input deposit and withdraw amount 
+    if (isValue == true) {
+        let currentBalanceTotalAmount = currentBalanceAmount + newInputValue;
+        currentBalance.innerText = currentBalanceTotalAmount;
+    }
+    else {
+        let currentBalanceTotalAmount = currentBalanceAmount - newInputValue;
+        currentBalance.innerText = currentBalanceTotalAmount;
+    }
+
+}
 // Get the deposit button 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const newInputValue = getInputValue('deposit-input')
     getTotal(newInputValue, 'deposit-total');
+    getTotalBalance(newInputValue, 'balance-total', true);
+})
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const newInputValue = getInputValue('withdraw-input');
+    getTotal(newInputValue, 'withdraw-total');
+    getTotalBalance(newInputValue, 'balance-total', false);
 })
